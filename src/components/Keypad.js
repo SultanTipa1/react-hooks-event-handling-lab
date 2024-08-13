3256b9b2072a44ded49d42cd17797f59a8f6d03c
SultanTipa1 <josephngumbaugush@gmail.com>
2024-08-13 12:51:52 +0300

    Adds all files

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
 // Code Keypad Component Here
+// components/Keypad.js
+import React from 'react';
 
-function Keypad (){
+function Keypad() {
+    const handleChange = () => {
+        console.log('Entering password...');
+    };
+
     return (
-        <div></div>
-    )
+        <input
+            type="password"
+            onChange={handleChange}
+        />
+    );
 }
 
 export default Keypad;
